f5466e46bf35823b364654a68e369ed57a10fbf2
Büşra Basan <124467787+Busrabasan67@users.noreply.github.com>
2024-07-31 21:18:53 +0300

    Grupları gösterecek dropdown oluşturuldu.

--- Takim_A/frontend/src/components/Sidebar.jsx
+++ Takim_A/frontend/src/components/Sidebar.jsx
@@ -4,16 +4,18 @@ import Search from './Search';
 import Chats from './Chats';
 
 const Sidebar = () => {
-  const chatData = [1, 2, 3, 4]; // Dummy data for example purposes
+  
   return (
-    <div className="flex flex-col lg:w-1/4 md:w-1/3 sm:w-1/2 w-full border-r border-gray-300 bg-[#3D3C61] h-screen p-4 overflow-y-auto">
+    <div className="flex flex-col lg:w-1/4 md:w-1/3 sm:w-1/2 w-full border-r border-gray-300 bg-[#3D3C61]  p-4 overflow-y-auto ">
       <Navbar/>
       <Search/>
       <Chats/>
       <Chats/>
       <Chats/>
       <Chats/>
-
+      <div className='bg-cyan-900'> 
+        deneme
+      </div>
       
       </div>
   )
