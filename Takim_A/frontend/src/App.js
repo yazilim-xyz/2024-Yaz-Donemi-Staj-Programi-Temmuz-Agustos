b9d871df7f027e7f3b0750f7a0efc80df8faeb43
Büşra Basan <124467787+Busrabasan67@users.noreply.github.com>
2024-08-02 18:21:15 +0300

    scrollbar daha iyi hale getirildi.

--- Takim_A/frontend/src/App.js
+++ Takim_A/frontend/src/App.js
@@ -3,9 +3,10 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './pages/Login';
 import Home from './pages/Home';
 import Chat from './pages/Chat';
-import GroupChat from './pages/GroupChat';
+//import GroupChat from './pages/GroupChat';
 import About from './components/About';
 import Contact from './components/Contact';
+import Groups from './components/Groups';
 
 
 
@@ -16,7 +17,7 @@ function App() {
         <Route path="/home" element={<Home />} />
         <Route path="/" element={<Login />} />
         <Route path="/messages" element={<Chat />}/>
-        <Route path="/GroupChat" element={<GroupChat />} />
+        <Route path="/groups" element={<Groups />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
