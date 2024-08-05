e941c7e1a067c0d5847e1d7a762f094c28af057b
tursuntalha <108190945+tursuntalha@users.noreply.github.com>
2024-08-05 16:04:50 +0300

    new Login , delete contact

--- Takim_A/frontend/src/pages/Login.js
+++ Takim_A/frontend/src/pages/Login.js
@@ -1,64 +1,168 @@
-import React, { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { signInWithEmailAndPassword } from 'firebase/auth';
-import { auth } from '../firebase'; 
-import logo from '../image/logo.jpg'; 
+import React, { useState, useEffect, useRef } from "react";
+import { useNavigate } from "react-router-dom";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../firebase";
+import logo from "../image/logo.jpg";
+import { FaArrowDown, FaLinkedin, FaGithub } from "react-icons/fa";
+import { RiInstagramFill } from "react-icons/ri";
+import { AiFillAliwangwang } from "react-icons/ai";
 
 const Login = () => {
   const navigate = useNavigate();
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const [isSmallScreen, setIsSmallScreen] = useState(window.innerWidth < 768);
+  const secondPageRef = useRef(null);
 
   useEffect(() => {
     const handleResize = () => setIsSmallScreen(window.innerWidth < 768);
 
-    window.addEventListener('resize', handleResize);
+    window.addEventListener("resize", handleResize);
 
     return () => {
-      window.removeEventListener('resize', handleResize);
+      window.removeEventListener("resize", handleResize);
     };
   }, []);
 
   const handleLogin = async () => {
     try {
       // Firebase ile kimlik doğrulama işlemi
-      const userCredential = await signInWithEmailAndPassword(auth, email, password);
+      const userCredential = await signInWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
       console.log("Giriş başarılı:", userCredential.user);
 
       // Başarıyla giriş yapıldığında yönlendirme
-      navigate('/home');
+      navigate("/home");
     } catch (error) {
       console.error("Giriş hatası:", error);
-      alert("Giriş başarısız. Lütfen bilgilerinizi kontrol edin ve tekrar deneyin.");
+      alert(
+        "Giriş başarısız. Lütfen bilgilerinizi kontrol edin ve tekrar deneyin."
+      );
+    }
+  };
+
+  const scrollToSecondPage = () => {
+    if (secondPageRef.current) {
+      secondPageRef.current.scrollIntoView({ behavior: "smooth" });
     }
   };
 
   return (
-    <div className="relative h-screen bg-cover bg-center" style={{ backgroundImage: 'url(https://i.pinimg.com/originals/ef/37/81/ef37815019ae52354c7c5772f4e886d6.png)' }}>
-      <div className="flex items-center justify-center h-full p-4">
-        <div className={`w-full max-w-5xl md:w-4/5 md:h-4/5 bg-secondary bg-opacity-50 text-white shadow-lg rounded-lg flex flex-col md:flex-row transition-transform duration-300 ${isSmallScreen ? 'transform scale-95' : 'transform scale-100'}`}>
-          <div className="w-full md:w-1/2 p-4 md:p-8 flex flex-col items-center justify-between bg-primary bg-opacity-80 text-primary relative">
-            <div className="flex flex-col items-center justify-center flex-grow">
+    <div className="h-screen overflow-auto snap-y snap-mandatory">
+      {/* Arka Plan ve Sayfa İçeriği */}
+      <div
+        className="section snap-start relative h-screen bg-cover bg-center flex flex-col justify-between"
+        style={{
+          backgroundImage:
+            "url(https://i.pinimg.com/originals/ef/37/81/ef37815019ae52354c7c5772f4e886d6.png)",
+          backgroundSize: "cover",
+          backgroundAttachment: "fixed",
+        }}
+      >
+        {/* WorkChat Başlığı ve Alt Çizgi */}
+        <div className="absolute top-0 left-0 right-0 flex flex-col items-center pt-8">
+          <div className="text-primary text-xl md:text-5xl font-bold text-center">
+            <h1>WorkChat</h1>
+            <hr className="border-primary border-t-4 my-3 w-1/2 mx-auto" />
+          </div>
+        </div>
+
+        {/* Diğer İçerik */}
+        <div className="flex flex-grow items-center justify-center pt-20">
+          <div className="flex flex-col md:flex-row items-center justify-center w-full text-center">
+            <div className="flex items-center justify-center md:w-1/2">
               <img
                 src={logo}
                 alt="WorkChat Logo"
-                className={`h-32 w-32 md:h-72 md:w-72 rounded-full border-4 border-black shadow-lg transition-transform duration-300 ${isSmallScreen ? 'transform scale-90' : 'transform scale-100'} mb-4`}
-                style={{ opacity: 0.9 }} 
+                className={`h-64 w-64 md:h-96 md:w-96 rounded-full border-4 border-black shadow-lg transition-transform duration-300 ${
+                  isSmallScreen ? "transform scale-90" : "transform scale-100"
+                }`}
+                style={{ opacity: 0.9 }}
               />
-              <p className="text-sm md:text-lg text-center text-white">İş yerinizde kolay ve hızlı iletişim kurun.</p>
             </div>
-            <div className="flex justify-between w-full absolute bottom-4 md:bottom-8 px-4 md:px-8">
-              <a href="/about" className="text-white text-sm md:text-lg hover:underline">Hakkımızda</a>
-              <a href="/contact" className="text-white text-sm md:text-lg hover:underline">İletişim</a>
+            <div className="md:w-1/2 mt-4 md:mt-0">
+              <div className="text-white text-xl md:text-2xl mt-4">
+                <p>İş yerinizde kolay ve hızlı iletişim kurun.</p>
+                <p>Yeni nesil iletişim araçlarıyla tanışın.</p>
+                <p>Verimliliğinizi artırın ve iş akışınızı kolaylaştırın.</p>
+              </div>
+              <div className="text-white text-2xl md:text-4xl font-bold mt-8 flex flex-col items-center">
+                <p>Keşfet</p>
+                <FaArrowDown
+                  onClick={scrollToSecondPage}
+                  className="text-white text-3xl cursor-pointer mt-4 transition-transform transform hover:scale-125 hover:bg-primary hover:p-2 rounded-full hover:text-white"
+                />
+              </div>
             </div>
           </div>
+        </div>
 
-          <div className="w-full md:w-1/2 p-4 md:p-8 flex items-center justify-center">
-            <div className="w-full max-w-md">
-              <h2 className="text-lg md:text-2xl font-bold mb-6 text-center text-white">Giriş Yap</h2>
+        {/* Saydam Footer */}
+        <div className="absolute bottom-0 left-0 right-0 bg-primary bg-opacity-50 py-8 flex justify-between items-center px-8">
+          <a
+            href="/about"
+            className="text-white text-sm md:text-lg hover:underline"
+          >
+            Hakkımızda
+          </a>
+          <div className="flex space-x-4">
+            <a
+              href="https://www.linkedin.com"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-white text-3xl font-bold transition-transform duration-300 transform hover:scale-125"
+            >
+              <FaLinkedin />
+            </a>
+            <a
+              href="https://www.instagram.com"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-white text-3xl font-bold transition-transform duration-300 transform hover:scale-125"
+            >
+              <RiInstagramFill />
+            </a>
+            <a
+              href="https://github.com"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-white text-3xl font-bold transition-transform duration-300 transform hover:scale-125"
+            >
+              <FaGithub />
+            </a>
+          </div>
+        </div>
+      </div>
+
+      {/* Giriş Formu Bölümü */}
+      <div
+        ref={secondPageRef}
+        className="section snap-start flex items-center justify-center h-screen bg-secondary"
+        style={{
+          backgroundImage:
+            "url(https://i.pinimg.com/originals/ef/37/81/ef37815019ae52354c7c5772f4e886d6.png)",
+          backgroundSize: "cover",
+          backgroundAttachment: "fixed",
+        }}
+      >
+        <div className="relative w-full max-w-4xl md:w-3/5 lg:w-3/5 h-[50vh] p-8 bg-primary bg-opacity-80 text-white shadow-lg rounded-lg flex flex-col">
+          <div className="flex flex-col items-center flex-grow justify-center pt-16">
+            <div className="w-full max-w-md text-center">
+              <div className="text-5xl mb-2 flex justify-center relative">
+                {/* Giriş Yap İkonu */}
+                <AiFillAliwangwang className="text-white transition-transform duration-300 transform hover:scale-125 hover:translate-x-4 hover:translate-y-4" style={{ position: "absolute", top: "-5rem" }} />
+              </div>
+              <h2 className="text-lg md:text-2xl font-bold mb-4 text-center">
+                Giriş Yap
+              </h2>
               <div className="mb-4">
-                <label className="block text-white text-sm font-bold mb-2" htmlFor="email">
+                <label
+                  className="block text-white text-sm font-bold mb-2"
+                  htmlFor="email"
+                >
                   Kullanıcı Adı
                 </label>
                 <input
@@ -71,7 +175,10 @@ const Login = () => {
                 />
               </div>
               <div className="mb-6">
-                <label className="block text-white text-sm font-bold mb-2" htmlFor="password">
+                <label
+                  className="block text-white text-sm font-bold mb-2"
+                  htmlFor="password"
+                >
                   Şifre
                 </label>
                 <input
