359d25f9f68628d4c240424efa6f1205e8bbca30
zeynepozsahin <ozsahinzeynep88@gmail.com>
2024-08-06 11:24:24 +0300

    sidebarhover

--- Takim_B/frontend/src/admin_panel/Sidebar.js
+++ Takim_B/frontend/src/admin_panel/Sidebar.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import { FaBox, FaPlus, FaEdit, FaTags, FaChartLine, FaSignOutAlt, FaUserCog } from "react-icons/fa";
+import { FaBox, FaPlus, FaTags, FaChartLine, FaSignOutAlt } from "react-icons/fa";
 
 function Sidebar({ setActivePage, activePage }) {
   const [isOpen, setIsOpen] = useState(true);
@@ -18,30 +18,29 @@ function Sidebar({ setActivePage, activePage }) {
       <ul className={`mt-4 ${isOpen ? 'block' : 'hidden'}`}>
         <li
           onClick={() => handleClick('categories-admin')}
-          className={`px-4 py-2 flex items-center cursor-pointer ${activePage === 'categories-admin' ? 'bg-gray-200 text-black' : 'text-white hover:bg-gray-200 hover:text-black'}`}
+          className={`px-4 py-2 flex items-center cursor-pointer transition-colors duration-200 ${activePage === 'categories-admin' ? 'bg-gray-200 text-black' : 'text-white hover:bg-green-500 hover:text-white hover:rounded-lg'}`}
         >
           <FaTags className="mr-2" /> {isOpen && 'Kategoriler'}
         </li>
         <li
           onClick={() => handleClick('productTable')}
-          className={`px-4 py-2 flex items-center cursor-pointer ${activePage === 'productTable' ? 'bg-gray-200 text-black' : 'text-white hover:bg-gray-200 hover:text-black'}`}
+          className={`px-4 py-2 flex items-center cursor-pointer transition-colors duration-200 ${activePage === 'productTable' ? 'bg-gray-200 text-black' : 'text-white hover:bg-green-500 hover:text-white hover:rounded-lg'}`}
         >
           <FaBox className="mr-2" /> {isOpen && 'Ürünler'}
         </li>
         <li
           onClick={() => handleClick('addProduct')}
-          className={`px-4 py-2 flex items-center cursor-pointer ${activePage === 'addProduct' ? 'bg-gray-200 text-black' : 'text-white hover:bg-gray-200 hover:text-black'}`}
+          className={`px-4 py-2 flex items-center cursor-pointer transition-colors duration-200 ${activePage === 'addProduct' ? 'bg-gray-200 text-black' : 'text-white hover:bg-green-500 hover:text-white hover:rounded-lg'}`}
         >
           <FaPlus className="mr-2" /> {isOpen && 'Ürün Ekle'}
         </li>
         <li
-          className={`px-4 py-2 flex items-center ${activePage === 'stock-tracking' ? 'bg-gray-200 text-black' : 'text-white hover:bg-gray-200 hover:text-black'}`}
+          className={`px-4 py-2 flex items-center transition-colors duration-200 ${activePage === 'stock-tracking' ? 'bg-gray-200 text-black' : 'text-white hover:bg-green-500 hover:text-white hover:rounded-lg'}`}
         >
           <FaChartLine className="mr-2" /> {isOpen && <Link to="/stock-tracking" onClick={() => handleClick('stock-tracking')}>Stok Takip</Link>}
         </li>
-
         <li
-          className={`px-4 py-2 flex items-center ${activePage === 'logout' ? 'bg-gray-200 text-black' : 'text-white hover:bg-gray-200 hover:text-black'}`}
+          className={`px-4 py-2 flex items-center transition-colors duration-200 ${activePage === 'logout' ? 'bg-gray-200 text-black' : 'text-white hover:bg-red-500 hover:text-white hover:rounded-lg'}`}
         >
           <FaSignOutAlt className="mr-2" /> {isOpen && <Link to="/" onClick={() => handleClick('logout')}>Çıkış Yap</Link>}
         </li>
